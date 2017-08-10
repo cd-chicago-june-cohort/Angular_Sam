64592f2f81c22a81a36fdf873d0049436d8261bd
sozo2 <sam.zoeller@gmail.com>
2017-08-10 14:29:51 -0500

    done with dojo email

--- myFirstAngularApp/src/app/app.component.ts
+++ myFirstAngularApp/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  emails = [
+    {
+      address: 'bill@gates.com',
+      importance: true,
+      subject: 'New Windows',
+      content: 'Windows XI will launch in year 2100.'
+    },
+    {
+      address: 'ada@lovelace.com',
+      importance: true,
+      subject: 'Programming',
+      content: 'Enchantress of Numbers'
+    },
+    {
+      address: 'john@carmac.com',
+      importance: false,
+      subject: 'Updated Algo',
+      content: 'New algorithm for shadow volumes.'
+    },
+    {
+      address: 'gabe@newel.com',
+      importance: false,
+      subject: 'HL3!',
+      content: 'Just kidding...'
+    }
+  ]
 }
